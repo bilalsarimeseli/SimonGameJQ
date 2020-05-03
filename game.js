28508fc24914139dad98f1db8c4376811751b9ac
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-05-03 14:24:27 -0700

    debugged level increment

--- game.js
+++ game.js
@@ -3,30 +3,30 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-//Like a switch for the game; we only call nextSequence() on the first keypress.
 var started = false;
-
-//Tracker
 var level = 0;
+
+//Start Switch and DOM update
 $(document).keypress(function () {
   if (!started) {
-    //Let's change the DOM to level-0
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
+//grabs the clicked button, animates, plays sounds functions
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
-  // When a user clicks on a button, the corresponding sound plays.
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
   checkAnswer(userClickedPattern.length - 1);
 });
 
+//Result Checking Logic
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     if (userClickedPattern.length === gamePattern.length) {
@@ -47,11 +47,11 @@ function checkAnswer(currentLevel) {
   }
 }
 
+//Animation in Sequence random
 function nextSequence() {
   userClickedPattern = [];
-  //Every time nextSequence is called, we increment the level by 1
   level++;
-  $("#text-title").text("Level " + level);
+  $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -60,16 +60,7 @@ function nextSequence() {
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-
-  //PlaySound() works for both playing sound in nextSequence() and when the user clicks a button.
   playSound(randomChosenColour);
-}
-
-//PlaySound() function takes a single input parameter "name".
-function playSound(name) {
-  //The code that plays sound in the nextSequence() function is added to playSound().
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
 }
 
 function animatePress(currentColor) {
@@ -77,6 +68,11 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
 function startOver() {
